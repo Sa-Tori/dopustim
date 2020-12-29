93dc7919d3ba6730a4fca721a0b6654e2b2fa23b
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2020-12-30 00:30:11 +0300

    Update reaction.js

--- reaction.js
+++ reaction.js
@@ -48,18 +48,49 @@ msg.react("768469943288332299");
         msg.react(reactionEmoji);
     };
     if (msg.author.id === "654810705903484949" && (getRandom(0, 4) == 1)) {
-      if (msg.content.toLowerCase().indexOf("Новый год идет") != -1) return;
+      if (msg.content.toLowerCase().indexOf("новый год идет") != -1) return;
         const tea = poetry.tea;
         const randomIndex = Math.floor(Math.random() * tea.length);
         const flot = tea[randomIndex];
         const reactionEmoji = client.emojis.cache.get(flot);
         msg.react(reactionEmoji);  
     };
-    if (msg.content.startsWith("Новый год идет") && msg.author.id === "654810705903484949") {
-        msg.react("768779785114484736");
-        msg.react("768779737765511169");
-        msg.react("768779760456302602");
-    };
+    if (msg.content.toLowerCase().indexOf("новый год") != -1) {
+        const rng = getRandom(0, 5);
+        if (rng == 1) {
+            msg.react("768779785114484736");
+            msg.react("768779737765511169");
+            msg.react("768779760456302602");
+        }
+        if (rng == 2) {
+            msg.react("789577059315613696");
+            msg.react("789577035898421258");
+            msg.react("789577009453858816");
+            msg.react("789576988112584704");
+            msg.react("789576942172372992");
+            msg.react("789576909108281346");
+        }
+        if (rng == 3) {
+            msg.react("789573460417314876");
+            msg.react("789573427669106709");
+            msg.react("789573398623551569");
+            msg.react("789573370861453313");
+            msg.react("789573337596166144");
+            msg.react("789573307594571787");
+            msg.react("789573263855583313");
+        }
+        if (rng == 4) {
+            msg.react("789566255996076032");
+            msg.react("789566219580735498");
+            msg.react("789566183707115530");
+        }
+        if (rng == 5) {
+            msg.react("789559203345858612");
+            msg.react("789559170767912970");
+            msg.react("789559132767125544");
+            msg.react("789559101205643294");
+        }
+    }; 
     if (msg.content.toLowerCase().indexOf("зажги") != -1 || msg.content.toLowerCase().indexOf("гори") != -1) {
         const ng = poetry.ng;
         const randomIndex = Math.floor(Math.random() * ng.length);
