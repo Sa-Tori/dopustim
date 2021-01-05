dbfb37d55627069943563d9fa159d952fd7413e5
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-01-05 12:20:12 +0300

    Delete answer.js

--- merlai/answer.js
+++
@@ -1,8 +0,0 @@
-const Discord2 = require("discord.js");
-const client2 = new Discord2.Client();
-
-client2.on ("ready", () => {
-    console.log("Ответы готовы");
-});
-
-client2.login(process.env.BOT_TOKEN2);
