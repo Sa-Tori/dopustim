bb8853d0622ca59d6827a18f1145cc4bfa1bd6f6
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-01-29 22:02:11 +0300

    Update dopustim.js

--- dopustim.js
+++ dopustim.js
@@ -40,6 +40,7 @@ const Discord2 = require("discord.js");
 const client2 = new Discord2.Client();
 
 //const answer = require("./merlai/answer.js");
+const reaction = require("./reaction");
 
 client2.on("ready", () => {
     console.log("Центр управления");
