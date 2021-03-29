e74d4dc19b67c2c1ec00af9aa471c34cc45d36fe
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-03-29 17:29:13 +0300

    Update answer.js

--- merlai/answer.js
+++ merlai/answer.js
@@ -2,6 +2,9 @@
 const Discord2 = require("discord.js");
 const client2 = new Discord2.Client();
 
+const poetry = require('./poetry.json');
+const alcohol = require('./poetry.json');
+
 client2.on("ready", () => {
     console.log('Ответы Мерлая готовы');
 });
@@ -16,10 +19,13 @@ client2.on("message", async (msg) => {
     } catch (err) { console.log(err); };
 });
 
-/*client2.on("message", (msg) => {
-  if (msg.content.toLowerCase().indexOf("умерла я")!=-1 && msg.author.id === "542663623789641729") {
-            msg.channel.send("Клянусь, нет её у меня!");
+client2.on("message", (msg) => {
+  if (msg.content.toLowerCase().indexOf("ча")!=-1 && msg.content.toLowerCase().indexOf(" ча")!=-1 && msg.author.id === "654810705903484949"){
+            const alcohol = poetry.alcohol;
+            const randomIndex = Math.floor(Math.random() * alcohol.length);
+            const word = alcohol[randomIndex];
+            msg.channel.send(word+" <:mme:625115196637315124>");
     };
-});*/
+});
 
 client2.login(process.env.BOT_TOKEN2);
