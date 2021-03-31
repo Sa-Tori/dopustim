e8dcd44c8476f2e38e0375a085541d726b5ab8cc
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-03-31 21:34:13 +0300

    Update reaction.js

--- merlai/reaction.js
+++ merlai/reaction.js
@@ -2,9 +2,16 @@
 const Discord2 = require("discord.js");
 const client2 = new Discord2.Client();
 
+const poetry = require('./poetry.json');
+const ahegao = require('./poetry.json');
+
 client2.on("ready", () => {
     console.log('Реакции Мерлая готовы');
 });
+
+function getRandom(l, r) {
+    return Math.round((Math.random() * r)) % (r - l) + l;
+}
 
 client2.on("message", (msg) => {
   if (msg.author.id === "523116257390886954" && msg.content.toLowerCase().indexOf("часть смерти явно")!=-1) { 
@@ -19,6 +26,14 @@ client2.on("message", (msg) => {
   if (msg.author.id === "523116257390886954" && msg.content.toLowerCase().indexOf("часть тебя безразлична")!=-1) { 
       msg.react("768469943288332299");
   };
+    if (msg.author.id === "542663623789641729" && (getRandom(0, 4) == 1) && msg.content.length == 0
+        && msg.channel.id == "804838051729637396" && msg.channel.id == "625758087554400266" && msg.channel.id == "816351964278292541" ) {
+        const ahegao = poetry.ahegao;
+        const randomIndex = Math.floor(Math.random() * ahegao.length);
+        const ah = ahegao[randomIndex];
+        msg.react(ah);
+    };
+});
 });
 
 client2.login(process.env.BOT_TOKEN2);
