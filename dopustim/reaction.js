214547d9d722778d84c87d26f0cef65313514f7a
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-04-01 20:19:17 +0300

    Update reaction.js

--- dopustim/reaction.js
+++ dopustim/reaction.js
@@ -101,7 +101,7 @@ msg.react("768469943288332299");
     if (msg.content.toLowerCase().indexOf("хлеп") != -1) {
         msg.channel.send("<a:g2774:760531480824512512>");
     };
-    if ((getRandom(0, 4) == 1) && msg.content.length == 0
+    if ((getRandom(0, 4) == 1) && msg.content.length == 0 && msg.channel.id == "523123642293420052"
         && msg.channel.id == "804838051729637396" && msg.channel.id == "625758087554400266" && msg.channel.id == "816351964278292541" ) {
         const ahegao = poetry.ahegao;
         const randomIndex = Math.floor(Math.random() * ahegao.length);
