dbf83edf6a22160934ea2a920a38cd5a5914fa02
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-04-05 19:49:15 +0300

    Update commands.js

--- merlai/commands.js
+++ merlai/commands.js
@@ -1,9 +1,9 @@
 'use strict';
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord2 = require("discord.js");
+const client2 = new Discord2.Client();
 
 client.on("ready", () => {
-    console.log("Команды Картуса готовы.");
+    console.log("Команды Мерлая готовы.");
     
 });
 
@@ -19,32 +19,10 @@ function say(msg) {
     }
 }
 
-client.on('message', (msg) => { // Реагирование на сообщения
-    const arggs = msg.content.split(' ').slice(1); // Все аргументы за именем команды с префиксом
-    const amount = arggs.join(' '); // Количество сообщений, которые должны быть удалены
-    if (msg.content.startsWith("delete") && msg.author.id === "542663623789641729") {
-        
-        if (!amount) return msg.channel.send('Вы не указали, сколько сообщений нужно удалить!'); // Проверка, задан ли параметр количества
-        if (isNaN(amount)) return msg.channel.send('Это не число!'); // Проверка, является ли числом ввод пользователя 
-
-        if (amount > 100) return msg.channel.send('Вы не можете удалить 100 сообщений за раз'); // Проверка, является ли ввод пользователя числом больше 100
-        if (amount < 1) return msg.channel.send('Вы должны ввести число больше чем 1'); // Проверка, является ли ввод пользователя числом меньше 1
-
-        async function delete_messages() { // Объявление асинхронной функции
-
-            await msg.channel.messages.fetch({
-                limit: amount
-            }).then(messages => {
-                msg.channel.bulkDelete(messages)
-                msg.channel.send(`Удалено ${amount} сообщений!`)
-            })
-        };
-        delete_messages();
-    };
-    
-    if (msg.content.startsWith("ksay") && msg.author.id === "542663623789641729") {
+client.on('message', (msg) => { 
+    if (msg.content.startsWith("msay") && msg.author.id === "542663623789641729") {
         say(msg);
     };
 });
 
-client.login(process.env.BOT_TOKEN);
+client2.login(process.env.BOT_TOKEN2);
