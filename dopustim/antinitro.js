8db4f0bff95a8eb4952fb97998fcb54e023286a4
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-04-13 22:24:52 +0300

    Create antinitro.js

---
+++ dopustim/antinitro.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("АнтиНитро Картуса готово.");
+    
+});
+
+
+
+client.on('message', (msg) => { 
+  
+});
+
+client.login(process.env.BOT_TOKEN);
