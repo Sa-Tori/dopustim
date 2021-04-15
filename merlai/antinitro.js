7afe0227c757bc126a74da1abf81000b431c1d41
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-04-15 11:44:47 +0300

    Update antinitro.js

--- merlai/antinitro.js
+++ merlai/antinitro.js
@@ -1,13 +1,23 @@
 const Discord2 = require("discord.js");
 const client2 = new Discord2.Client();
+const { Client, MessageEmbed } = require('discord.js');
+const poetry = require('./poetry.json');
 
 client2.on("ready", () => {
     console.log("АнтиНитро Мерлая готово.");
     
 });
 
+/*if (msg.content === "" || msg.content === "") {
+        msg.delete();
+        msg.channel.send("");
+    };*/
 
 client2.on('message', (msg) => { 
+if (msg.content === "мер" || msg.content === "Мер") {
+        msg.delete();
+        msg.channel.send("<:merlai:826871930325303328>");
+    };
     
 });
 
