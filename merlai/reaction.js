266bbb1747f91309420b9e8bf8d13714427ae021
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-08-15 03:14:56 +0300

    Update reaction.js

--- merlai/reaction.js
+++ merlai/reaction.js
@@ -36,10 +36,13 @@ client2.on("message", (msg) => {
         msg.react(ah);
     };
     if ((msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649") && msg.content.toLowerCase().indexOf(" присутствия души")!=-1) { 
-        msg.react("768469879211687936");
+        msg.react("837282717463871489");
+        msg.react("837281812211433472");
+        msg.react("837281979773878293");
         msg.react("768469897398190081");
-        msg.react("768469924401250325");
-        msg.react("768469943288332299");
+        msg.react("872262702389669969");
+        msg.react("526735399314391040");
+        msg.react("837282591996248075");
     };
     if (msg.author.id === "542663623789641729" && (getRandom(0, 17) == 7)) {
         const fire = poetry.fire;
