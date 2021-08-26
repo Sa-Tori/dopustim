9122c98646394c7f5d8a02f667a7f7577a9ca4ec
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-08-26 20:54:28 +0300

    Update commands.js

--- dopustim/commands.js
+++ dopustim/commands.js
@@ -35,32 +35,49 @@ function say(msg) {
 	}
 }
 
+function del(msg) {
+	try {
+		let chen = msg.channel.id;
+		let p1 = msg.content.indexOf(' ');
+		let mes_id = msg.content.substring(p1 + 1);
+		let channel = client.channels.cache.get(chen);
+		var message = channel.messages.cache.get(mes_id);
+		message.delete();
+		msg.delete();
+	} catch {
+		msg.reply("ERROR!!");
+	}
+}
+
 client.on('message', (msg) => { // Реагирование на сообщения
-    const arggs = msg.content.split(' ').slice(1); // Все аргументы за именем команды с префиксом
-    const amount = arggs.join(' '); // Количество сообщений, которые должны быть удалены
-    if (msg.content.startsWith("delete") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
-        
-        if (!amount) return msg.channel.send('Вы не указали, сколько сообщений нужно удалить!'); // Проверка, задан ли параметр количества
-        if (isNaN(amount)) return msg.channel.send('Это не число!'); // Проверка, является ли числом ввод пользователя 
+	    const arggs = msg.content.split(' ').slice(1); // Все аргументы за именем команды с префиксом
+	    const amount = arggs.join(' '); // Количество сообщений, которые должны быть удалены
+	    if (msg.content.startsWith("delete") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
 
-        if (amount > 100) return msg.channel.send('Вы не можете удалить 100 сообщений за раз'); // Проверка, является ли ввод пользователя числом больше 100
-        if (amount < 1) return msg.channel.send('Вы должны ввести число больше чем 1'); // Проверка, является ли ввод пользователя числом меньше 1
+		if (!amount) return msg.channel.send('Вы не указали, сколько сообщений нужно удалить!'); // Проверка, задан ли параметр количества
+		if (isNaN(amount)) return msg.channel.send('Это не число!'); // Проверка, является ли числом ввод пользователя 
 
-        async function delete_messages() { // Объявление асинхронной функции
+		if (amount > 100) return msg.channel.send('Вы не можете удалить 100 сообщений за раз'); // Проверка, является ли ввод пользователя числом больше 100
+		if (amount < 1) return msg.channel.send('Вы должны ввести число больше чем 1'); // Проверка, является ли ввод пользователя числом меньше 1
 
-            await msg.channel.messages.fetch({
-                limit: amount
-            }).then(messages => {
-                msg.channel.bulkDelete(messages)
-                msg.channel.send(`Удалено ${amount} сообщений!`)
-            })
-        };
-        delete_messages();
-    };
+		async function delete_messages() { // Объявление асинхронной функции
+
+		    await msg.channel.messages.fetch({
+			limit: amount
+		    }).then(messages => {
+			msg.channel.bulkDelete(messages)
+			msg.channel.send(`Удалено ${amount} сообщений!`)
+		    })
+		};
+		delete_messages();
+	   };
     
-    if (msg.content.startsWith("ksay") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
-        say(msg);
-    };
+    	if (msg.content.startsWith("ksay") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
+        	say(msg);
+    	};
+	if (msg.content.startsWith("удали") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
+		del(msg);
+	};
 });
 
 
