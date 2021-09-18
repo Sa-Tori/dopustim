45433dae08964f5e4e6e1d25ba30b25879bcb704
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-09-18 13:33:18 +0300

    Update replicas.js

--- merlai/replicas.js
+++ merlai/replicas.js
@@ -49,7 +49,7 @@ client2.on("message", (msg) => {
 
 		let new_arr = arr.splice(random(max), len)
 		let new_str = new_arr.join(" ");
-		if (getRandom(0, 61) == 49 && msg.channel.id != "523123642293420052" || fl == true) {
+		if (getRandom(0, 99) == 49 && msg.channel.id != "523123642293420052" || fl == true) {
 			const embed = new MessageEmbed()
 				.setAuthor(UserTag, msg.author.displayAvatarURL({ dynamic: true }))
 				.setDescription("**Текст:** `"+new_str+"`\n**Исходный текст:** `"+str+"`")
