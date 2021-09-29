bf6bb6ff0c459962d693d26e7b24abc0162ceafb
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-09-30 00:41:05 +0300

    Update worker.js

--- merlai/worker.js
+++ merlai/worker.js
@@ -1,12 +1,43 @@
 'use strict';
 const Discord2 = require("discord.js");
 const client2 = new Discord2.Client();
+const poetry = require('./poetry.json');
+const blya = poetry.blya;
+const anch = poetry.anch;
 
 //сообщения
 
 client2.on("ready", () => {
     console.log('Работа Мерлая готова');
 });
+
+function isFinded(s, morn) {
+	for (var i = 0; i < morn.length; i++) {
+		if (s.toLowerCase().indexOf(morn[i]) != -1) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function anblya(msg) {
+	try {
+		var str = msg.content;
+		for (let i = 0; i < blya.length; i++) {
+			if (str.toLowerCase().indexOf(blya[i]) != -1) {
+				var lt = str.length;
+				let w = blya[i];
+				var lb = w.length;
+				str = str.replace(w, '<:blyat:838469432103141377>');
+			};
+		};
+		if (lt != lb) msg.reply('материться запрещено. Цензура:');
+		msg.channel.send(str);
+		msg.delete();
+	} catch {
+		msg.reply("ERROR!!");
+	}
+}
 
 client2.on('message', async (msg) => {
     let bump = client2.channels.cache.get('570922055407501332');
@@ -17,6 +48,15 @@ client2.on('message', async (msg) => {
             bump.send('<@&891331557300764712> Пора бампать!');
         }
     } catch (err) { console.log(err); };
+});
+
+client2.on('message', (msg) => {
+    if (isFinded(msg.content, blya) && msg.guild.id == "532954366571708427") {
+		for (let i = 0; i < anch.length; i++) {
+			if (msg.channel.id === anch[i]) return;
+		};
+		anblya(msg);
+	};
 });
 
 client2.login(process.env.BOT_TOKEN2);
