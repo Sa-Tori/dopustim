6e97d0fdf26f9affa1105cc4d4b94b82b12fdbcd
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-09-30 10:46:41 +0300

    Update replicas.js

--- merlai/replicas.js
+++ merlai/replicas.js
@@ -65,26 +65,5 @@ client2.on("message", (msg) => {
 });
 
 
-client2.on('messageDelete', async msg => {
-	let control = client2.channels.cache.get('878520465856036935');
-	if (msg.author.id === "776445694587306028") {
-		if (!msg.guild) return;
-		const fetchedLogs = await msg.guild.fetchAuditLogs({
-			limit: 1,
-			type: 'MESSAGE_DELETE',
-		});
-		const deletionLog = fetchedLogs.entries.first();
-		if (!deletionLog) return control.send(`Сообщение от ${msg.author.tag} был удален, но соответствующие журналы аудита не были найдены.`);
-
-		const { executor, target } = deletionLog;
-		if (target.id === msg.author.id && executor.id != "776445694587306028") {
-			control.send(`Сообщение от ${msg.author.tag} было удалено ${executor.tag}.`);
-			msg.channel.send(`Какой-то пидор удалил мое сообщение. <:mme:625115196637315124>\nА, постойте, это же ${executor.tag} <:096:592420847017263134>`);
-		} else {
-			control.send(`Сообщение от ${msg.author.tag} было удалено, но мы не знаем, кем.`);
-		};
-	};
-});
-
 
 client2.login(process.env.BOT_TOKEN2);
