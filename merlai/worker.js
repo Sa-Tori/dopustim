cc0433a82c3f63f51870e8f90d8885a3f4973202
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-09-30 08:14:09 +0300

    Update worker.js

--- merlai/worker.js
+++ merlai/worker.js
@@ -1,4 +1,4 @@
-'use strict'; 
+'use strict';
 const Discord2 = require("discord.js");
 const client2 = new Discord2.Client();
 const poetry = require('./poetry.json');
@@ -8,7 +8,7 @@ const anch = poetry.anch;
 //сообщения
 
 client2.on("ready", () => {
-    console.log('Работа Мерлая готова');
+	console.log('Работа Мерлая готова');
 });
 
 function isFinded(s, morn) {
@@ -25,6 +25,7 @@ function anblya(msg) {
 		var str = msg.content;
 		for (let i = 0; i < blya.length; i++) {
 			if (str.toLowerCase().indexOf(blya[i]) != -1) {
+				str = str.toLowerCase();
 				var lt = str.length;
 				let w = blya[i];
 				var lb = w.length;
@@ -40,23 +41,24 @@ function anblya(msg) {
 }
 
 client2.on('message', async (msg) => {
-    let bump = client2.channels.cache.get('570922055407501332');
-    try {
-        if ((msg.embeds.length > 0 && msg.embeds[0].description.toLowerCase().indexOf("server bumped by") != -1 && msg.guild.id == "532954366571708427") || (msg.author.id == "542663623789641729" && msg.content == "обновить таймер")) {
-            let delay = async (duration) => { await new Promise(resolve => setTimeout(resolve, duration)) };
-            await delay(4 * 60 * 60 * 1000);
-            bump.send('<@&891331557300764712> Пора бампать!');
-        }
-    } catch (err) { console.log(err); };
+	let bump = client2.channels.cache.get('570922055407501332');
+	try {
+		if ((msg.embeds.length > 0 && msg.embeds[0].description.toLowerCase().indexOf("server bumped by") != -1 && msg.guild.id == "532954366571708427") || (msg.author.id == "542663623789641729" && msg.content == "обновить таймер")) {
+			let delay = async (duration) => { await new Promise(resolve => setTimeout(resolve, duration)) };
+			await delay(4 * 60 * 60 * 1000);
+			bump.send('<@&891331557300764712> Пора бампать!');
+		}
+	} catch (err) { console.log(err); };
 });
 
 client2.on('message', (msg) => {
-    if (isFinded(msg.content, blya) && msg.guild.id == "532954366571708427") {
+	if (isFinded(msg.content, blya) && msg.guild.id == "532954366571708427" && !msg.author.bot) {
 		for (let i = 0; i < anch.length; i++) {
 			if (msg.channel.id === anch[i]) return;
 		};
-		//anblya(msg);
+		anblya(msg);
 	};
 });
+
 
 client2.login(process.env.BOT_TOKEN2);
