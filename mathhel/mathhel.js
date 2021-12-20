f38ea4a461ef816f582b626ad1fb255f0e39ce2e
Sa-Tori <72759640+Sa-Tori@users.noreply.github.com>
2021-12-21 02:44:15 +0300

    Update mathhel.js

--- mathhel/mathhel.js
+++ mathhel/mathhel.js
@@ -4,6 +4,8 @@ const client4 = new Discord4.Client();
 const { Client, MessageEmbed } = require('discord.js');
 
 const poetry = require('./poetry.json');
+var event = '';
+var point = '';
 
 client4.on("ready", () => {
 	console.log("Приложение подключено");
@@ -65,79 +67,105 @@ function del(msg) {
 
 //_______________________________команда delete для удаления сообщений
 
-client4.on('message', (msg) => { 
-	const arggs = msg.content.split(' ').slice(1); 
-	const amount = arggs.join(' '); 
-	if (msg.content.startsWith("delete") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
+client4.on('message', async (msg) => {
+	event = 'message';
+	const arggs = msg.content.split(' ').slice(1);
+	const amount = arggs.join(' ');
+	try {
+		point = 'команда delete';
+		if (msg.content.startsWith("delete") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
 
-		if (!amount) return msg.channel.send('Вы не указали, сколько сообщений нужно удалить!'); 
-		if (isNaN(amount)) return msg.channel.send('Это не число!'); 
+			if (!amount) return msg.channel.send('Вы не указали, сколько сообщений нужно удалить!');
+			if (isNaN(amount)) return msg.channel.send('Это не число!');
 
-		if (amount > 100) return msg.channel.send('Вы не можете удалить 100 сообщений за раз'); 
-		if (amount < 1) return msg.channel.send('Вы должны ввести число больше чем 1'); 
+			if (amount > 100) return msg.channel.send('Вы не можете удалить 100 сообщений за раз');
+			if (amount < 1) return msg.channel.send('Вы должны ввести число больше чем 1');
 
-		async function delete_messages() {
+			async function delete_messages() {
 
-			await msg.channel.messages.fetch({
-				limit: amount
-			}).then(messages => {
-				msg.channel.bulkDelete(messages)
-				msg.channel.send(`Удалено ${amount} сообщений!`)
-			})
+				await msg.channel.messages.fetch({
+					limit: amount
+				}).then(messages => {
+					msg.channel.bulkDelete(messages)
+					msg.channel.send(`Удалено ${amount} сообщений!`)
+				})
+			};
+			delete_messages();
 		};
-		delete_messages();
+	} catch (err) {
+		let center = client4.channels.cache.get('921686023812022272');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
+		console.log(err);
 	};
 
-//_______________________________вызов функции "say"
-
-	if (msg.content.startsWith("m!say") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
-		say(msg);
+	//_______________________________вызов функции "say"
+	try {
+		point = 'команда say';
+		if (msg.content.startsWith("m!say") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
+			say(msg);
+		};
+	} catch (err) {
+		let center = client4.channels.cache.get('921686023812022272');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
+		console.log(err);
 	};
-
 });
 
 //_______________________________вызов функции "del"
 
 client4.on('message', (msg) => {
-	if (msg.content.startsWith("m!del") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
-		del(msg);
+	event = 'message';
+	try {
+		point = 'команда del';
+		if (msg.content.startsWith("m!del") && (msg.author.id === "542663623789641729" || msg.author.id === "478669590365339649")) {
+			del(msg);
+		};
+	} catch (err) {
+		let center = client4.channels.cache.get('921686023812022272');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
+		console.log(err);
 	};
 });
 
 //_______________________________Приветствие новых пользователей
 
-client4.on('guildMemberAdd', async (member) => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'гостевая');
-    try {
-    if (!channel) return;
-        channel.send(`${member} добро пожаловать на сервер ${guild} <:ezgif1:840339018453090306>`);
-    } catch (err) {
-        let center = client4.channels.cache.get('921686023812022272');
-        center.send('Ошибка события: guildMemberAdd');
-        console.log(err);
-    };
+client4.on('guildMemberAdd', (member) => {
+	event = 'guildMemberAdd';
+	const channel = member.guild.channels.cache.get('921684420413493250');
+	try {
+		point = 'Приветствие';
+		if (!channel) return;
+		channel.send(`${member} добро пожаловать на сервер!`);
+	} catch (err) {
+		let center = client4.channels.cache.get('921686023812022272');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
+		console.log(err);
+	};
 });
 
 //_______________________________Сообщение об уходе пользователей
 
 client4.on('guildMemberRemove', async (member) => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'гостевая');
-    let UserTag = member.user.tag;
-    if (!channel) return;
-    try {
-        if (channel) {
-            channel.send("Пользователь " + UserTag + " покинул сервер.");
-        }
-    } catch (err) {
-        let center = client4.channels.cache.get('921686023812022272');
-        center.send('Ошибка события: guildMemberRemove');
-        console.log(err);
-    };
+	event = 'guildMemberRemove';
+	const channel = member.guild.channels.cache.get('921684420413493250');
+	let UserTag = member.user.tag;
+	if (!channel) return;
+	try {
+		point = '';
+		if (channel) {
+			channel.send("Пользователь " + UserTag + " покинул сервер.");
+		}
+	} catch (err) {
+		let center = client4.channels.cache.get('921686023812022272');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
+		console.log(err);
+	};
 });
 
 //_______________________________автоответчик
 
 client4.on("message", (msg) => {
+	event = 'message';
 	if (msg.content.startsWith("ping")) {
 		msg.channel.send("pong");
 	};
@@ -152,8 +180,10 @@ client4.on("message", (msg) => {
 //_______________________________вывод аватара пользователя
 
 client4.on('message', async (msg) => {
+	event = 'message';
 	let UserTag = msg.author.tag;
 	try {
+		point = '';
 		if (msg.content.startsWith('ava')) {
 			let emb;
 			let s = msg.content;
@@ -163,24 +193,24 @@ client4.on('message', async (msg) => {
 					id = id.substring(1);
 				let us = client4.users.cache.get(id);
 				emb = new MessageEmbed()
-					.setTitle("Аватар пользователя " + us)
+					.setTitle("Аватар пользователя " + us.tag)
 					.setAuthor(UserTag, msg.author.displayAvatarURL({ dynamic: true }))
 					.setColor(0xd38247)
-					.setTimestamp() 
+					.setTimestamp()
 					.setImage(us.avatarURL({ dynamic: true }));
 			} else {
 				emb = new MessageEmbed()
 					.setTitle("Ваш аватар")
 					.setAuthor(UserTag, msg.author.displayAvatarURL({ dynamic: true }))
 					.setColor(0xd38247)
-					.setTimestamp() 
+					.setTimestamp()
 					.setImage(msg.author.displayAvatarURL({ dynamic: true }));
 			}
 			msg.reply(emb)
 		}
 	} catch (err) {
 		let center = client4.channels.cache.get('921686023812022272');
-		center.send('Ошибка события: message\nКоманда: ava');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
 		console.log(err);
 	};
 });
@@ -188,7 +218,9 @@ client4.on('message', async (msg) => {
 ////_______________________________новогодние реакции
 
 client4.on('message', async (msg) => {
+	event = 'message';
 	try {
+		point = 'новогодние реакции';
 		if (msg.content.toLowerCase().indexOf("нов") != -1 && msg.content.toLowerCase().indexOf("год") != -1) {
 			const rng = getRandom(1, 6);
 			if (rng == 1) {
@@ -226,9 +258,10 @@ client4.on('message', async (msg) => {
 			}
 		};
 
-//_______________________________триггер "гирлянда" из эмодзи
+		//_______________________________триггер "гирлянда" из эмодзи
 
 		if (msg.content.toLowerCase().indexOf("зажги") != -1 || msg.content.toLowerCase().indexOf("гори") != -1) {
+			point = 'гирлянда';
 			const ng = poetry.ng;
 			const randomIndex = Math.floor(Math.random() * ng.length);
 			const emoji = ng[randomIndex];
@@ -236,8 +269,151 @@ client4.on('message', async (msg) => {
 		};
 	} catch (err) {
 		let center = client4.channels.cache.get('921686023812022272');
-		center.send('Ошибка события: message\nТип: Гирлянда');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
 		console.log(err);
+	};
+});
+
+//_______________________________функция понижения регистра
+
+function lowerCase(s) {
+	var s1 = "";
+	for (var i = 0; i < s.length; i++) {
+		if (s.charCodeAt(i) == 1025) {
+			s1 += "е";
+			continue;
+		} else if (s.charCodeAt(i) >= 1040 && s.charCodeAt(i) <= 1071) {
+			s1 += String.fromCharCode(s.charCodeAt(i) + 32);
+			continue;
+		} else
+			s1 += s.charAt(i);
+	}
+	return s1;
+}
+
+//_______________________________функция изменения типа
+
+function isDigit(s) {
+	if (s.charCodeAt(0) >= 97 && s.charCodeAt(0) <= 122)
+		return true;
+	if (s.charCodeAt(0) >= 1072 && s.charCodeAt(0) <= 1103)
+		return true;
+	return false;
+}
+
+//_______________________________функция случайного выбора слова
+
+function rand_word(s) {
+	var a = [];
+	s = lowerCase(s);
+	while (s.length > 0) {
+		while (s.length > 0 && !isDigit(s.charAt(0))) {
+			s = s.substr(1, s.length - 1);
+		}
+		var z = "";
+		var i = 0;
+		if (s.length == 0)
+			break;
+		while (i < s.length && isDigit(s.charAt(i))) {
+			z = z + s.charAt(i);
+			i++;
+		}
+		if (z != "")
+			a.push(z);
+		s = s.substring(i, s.length);
+	}
+	var e = getRandom(0, a.length);
+	return a[e];
+}
+
+//_______________________________отправка случайного определения по соответствию фрагмента строки
+
+client4.on("message", async (msg) => {
+	event = 'message';
+	try {
+		point = 'термины';
+		if ((msg.guild.id == "921684419633360896" || msg.content.startsWith('term')) && msg.author.id != "523116257390886954") {
+			var m = msg.content;
+			var fl = 0;
+			if (msg.content.startsWith('term')) {
+				m = msg.content.substring(3, msg.content.length);
+				fl = 1;
+			}
+			var rd = rand_word(m);
+			var b = [];
+			var terms = poetry.terms;
+			for (var i = 0; i < terms.length; i++)
+				if (lowerCase(terms[i]).includes(rd))
+					b.push(terms[i]);
+			if (b.length != 0 && (getRandom(0, 100) == 72 || fl == 1)) {
+				var t = getRandom(0, b.length);
+				msg.channel.send("```" + b[t] + "```");
+			}
+			if (fl == 1 && b.length == 0) {
+				msg.channel.send("Не могу найти подходящего определения.");
+			}
+		}
+	} catch (err) {
+		let center = client4.channels.cache.get('921686023812022272');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
+		console.log(err);
+	};
+});
+
+//_______________________________вывод информации об удаленных сообщениях
+
+client4.on('messageDelete', async msg => {
+	event = 'messageDelete';
+	try {
+		point = 'удаление сообщений';
+		let deletin = client4.channels.cache.get('922181203300335776');
+		let UserTag = msg.author.tag;
+		const embed = new MessageEmbed()
+			.setAuthor(UserTag, msg.author.displayAvatarURL({ dynamic: true }))
+			.setDescription("**Текст:** `" + msg.content + "`")
+			.addFields({
+				name: 'Данные',
+				value: "**Канал:** `" + msg.channel.name + "`\n**id автора:** `" + msg.author + "`"
+			})
+			.setColor(0xb60808)
+		deletin.send(embed);
+	} catch (err) {
+		let center = client4.channels.cache.get('921686023812022272');
+		center.send('Ошибка события: ' + event + '\nНазначение: ' + point);
+		console.log(err);
+	};
+});
+
+//_______________________________меню и список команд и действий
+
+client4.on("message", (msg) => {
+	if (msg.content.toLowerCase().indexOf("m!help") != -1) {
+		const embed = new MessageEmbed()
+			.setTitle("Меню")
+			.setDescription("**m!help** - вывод списка меню.\n" + 
+				"**delete** - команда удаления нескольких сообщений.\n" +
+				"**m!del** - команда удаления одного сообщения.\n" +
+				"**m!say** - команда повторить сообщение.\n" +
+				"**ping** - pong\n" +
+				"**ava** - вывод аватара пользователя.\n" +
+				"**Новый год** - новогодняя гирлянда из реакций.\n" +
+				"**зажги / гори** - новогодняя гирлянда из emoji.\n" +
+				"**term** - запрос математического определения.\n" +
+				"**Масхел?** - вопрос боту Масхел.\n" +
+				"**m!list** - список других возможностей Масхел.")
+			.setColor(0xd38247);
+		msg.channel.send(embed);
+	};
+	if (msg.content.toLowerCase().indexOf("m!list") != -1) {
+		const embed = new MessageEmbed()
+			.setTitle("Список возможностей")
+			.setDescription("**1.** Приветствие новых пользователей сервера.\n" +
+				"**2.** Уведомление о выходе пользовотелей с сервера.\n" +
+				"**3.** Вывод информации об удаленных сообщениях.\n" +
+				"**4.** Случайный вывод случайных математических определений.\n" +
+				"**5.** Вывод информации в чат-консоль *'center'*.\n")
+			.setColor(0xd38247);
+		msg.channel.send(embed);
 	};
 });
 
